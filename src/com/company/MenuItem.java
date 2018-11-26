4ab801d30cc3981f81adfbfcb4703890893344a5
BT_1N3_21@BKTECH.LOCAL <BT_1N3_21@BKTECH.LOCAL>
2018-11-26 08:41:38 -0500

    Initial commit

---
+++ src/com/company/MenuItem.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface MenuItem
+{
+   String getName();
+   double getPrice();
+
+}
---
+++ src/com/company/MenuRunner.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MenuRunner {
+
+    public static void main(String[] args) {
+	// write your code here
+        Sandwich reuben = new Sandwich("Reuben",5.75 );
+        Sandwich bec = new Sandwich( "BEC",2.00);
+        Salad waldorf = new Salad("Waldorf",7.25);
+        Salad caesar = new Salad("Caesar",4.50);
+        Drink icedTea = new Drink()
+    }
+}
