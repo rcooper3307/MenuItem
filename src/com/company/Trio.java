fb0fe677b30940db3d4e3ad6f0bd2911e485c3e5
rcooper3307 <rcooper3307@bths.edu>
2018-11-27 08:41:41 -0500

    sdf

--- src/com/company/Trio.java
+++ src/com/company/Trio.java
@@ -2,21 +2,39 @@ package com.company;
 
 public class Trio implements MenuItem
 {
-    private String name;
-    private double price;
-
-    public Trio(String name, double price)
+  Sandwich sandwich;
+  Salad salad;
+  Drink drink;
+    public Trio(Sandwich sandwich,Salad salad,Drink drink)
     {
-        this.name = name;
-        this.price = price;
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
     }
+
     public String getName()
     {
-        return name;
+        return sandwich.getName()+ "/"+ salad.getName()+"/"+drink.getName();
     }
     public double getPrice()
     {
-        return price;
+        double Price = 0;
+        if (sandwich.getPrice() > salad.getPrice() && drink.getPrice() > salad.getPrice())
+        {
+            Price = sandwich.getPrice() + drink.getPrice();
+        }
+        if (salad.getPrice() > sandwich.getPrice() && drink.getPrice() > sandwich.getPrice())
+        {
+            Price = sandwich.getPrice() + drink.getPrice();
+        }
+        if (sandwich.getPrice() > salad.getPrice() && drink.getPrice() > salad.getPrice())
+        {
+            Price = sandwich.getPrice() + drink.getPrice();
+        }if (sandwich.getPrice() > drink.getPrice() && salad.getPrice() > drink.getPrice())
+    {
+        Price = sandwich.getPrice() + salad.getPrice();
+    }
+        return Price;
     }
     public String toString()
     {
